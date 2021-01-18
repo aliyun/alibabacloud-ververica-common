5fe4530242f5019129707eefc9099f8b65a40691
yuanqi <yuanqi@alibaba-inc.com>
2021-01-18 11:42:31 +0800

    Fix problem when generating java doc

--- src/main/java/com/ververica/common/model/alarmrule/Comparators.java
+++ src/main/java/com/ververica/common/model/alarmrule/Comparators.java
@@ -1,9 +1,7 @@
 package com.ververica.common.model.alarmrule;
 
 public enum Comparators {
-  /** The alert content has an operator，expression: ">=" */
   CURRENT_GTE,
 
-  /** The alert content has an operator，expression: "<=" */
   CURRENT_LTE,
 }

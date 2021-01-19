30b4fd87fd0911e89913c9e33f1fdf9799eaa1af
wanglufeng-wlf <74338448+wanglufeng-wlf@users.noreply.github.com>
2021-01-19 14:04:14 +0800

    Add validation for alarms Api,change the version number of Common to 1.0.2 (#10)

--- src/main/java/com/ververica/common/model/alarmrule/NotifyRule.java
+++ src/main/java/com/ververica/common/model/alarmrule/NotifyRule.java
@@ -1,15 +1,18 @@
 package com.ververica.common.model.alarmrule;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class NotifyRule {
-  /** This is the alarm interval，Supports 1 day and 1 minute */
+  /** This is the alarm interval，Supports 1 day and 1 minute,The parameter must be '1' or '1440' */
   @JsonProperty("alarm_interval")
-  AlarmInterval alarmInterval;
+  @NotBlank(message = "alarm_interval not set")
+  String alarmInterval;
   /** This is the effective time, e.d."0:0-23:59" */
   @JsonProperty("time_range")
+  @NotBlank(message = "time_range not set")
   String timeRange;
   /** This is the Inform the way */
   Critical critical;

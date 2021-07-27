d533d661a11c3ff3758f1728d0e8a2fd0a957b05
wanglufeng <wb-wlf721952@alibaba-inc.com>
2021-07-27 15:18:13 +0800

    Adds parameters to Deployment class, Set Set ValidateSqlScriptParams to be Deprecated, change the version number of Common to 1.0.15.

--- src/main/java/com/ververica/common/model/deployment/ValidationError.java
+++ src/main/java/com/ververica/common/model/deployment/ValidationError.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class ValidationError {
-  private final String attribute;
-  private final String message;
-  private final ValidationErrorReason reason;
+  String attribute;
+  String message;
+  ValidationErrorReason reason;
 }
